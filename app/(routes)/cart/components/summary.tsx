2102f57416e8361c699298e22050414de6c9d00e
louis.le@kmail.vn <khoe.le.mall@gmail.com>
2023-07-24 17:54:35 +0700

    #feat: loading for Checkout

--- app/(routes)/cart/components/summary.tsx
+++ app/(routes)/cart/components/summary.tsx
@@ -10,6 +10,7 @@ import useCart from "@/hooks/use-cart";
 import { toast } from "react-hot-toast";
 
 const Summary = () => {
+  const [loading , setLoading] = useState<boolean>(false);
   const searchParams = useSearchParams();
   const items = useCart((state) => state.items);
   const removeAll = useCart((state) => state.removeAll);
@@ -30,6 +31,7 @@ const Summary = () => {
   }, 0);
 
   const onCheckout = async () => {
+    setLoading(true)
     const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/checkout`, {
       productIds: items.map((item) => item.id)
     });
@@ -37,7 +39,7 @@ const Summary = () => {
     window.location = response.data.url;
   }
 
-  return ( 
+  return (
     <div
       className="mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8"
     >
@@ -51,10 +53,28 @@ const Summary = () => {
         </div>
       </div>
       <Button onClick={onCheckout} disabled={items.length === 0} className="w-full mt-6">
-        Checkout
+        {loading ? (
+            <div className='flex justify-center cursor-wait'>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6 animate-spin"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
+                />
+              </svg>
+              <p> Loading....</p>
+            </div>
+          ) : "Checkout"}
       </Button>
     </div>
   );
 }
- 
+
 export default Summary;
