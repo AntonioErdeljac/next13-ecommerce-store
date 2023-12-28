19524ff04fd9ad56c6817a882b25c2d9931756c8
sulaebha333 <150430290+sulaebha333@users.noreply.github.com>
2023-12-28 13:54:38 +0530

    Update page.tsx

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -8,7 +8,7 @@ export const revalidate = 0;
 
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true });
-  const billboard = await getBillboard("0e12e5cf-29ab-4529-b8d5-c5371dae1f7b");
+  const billboard = await getBillboard("19cbf46f-60a4-441b-9a57-71c3e7752f25");
 
   return (
     <Container>
